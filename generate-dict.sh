20be4630780ee33079c36fd754e3176c1ad2f97e
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-08-05 21:44:48 +0200

    Fix typescript error

--- generate-dict.sh
+++ generate-dict.sh
@@ -1,1 +1,1 @@
-npx ts-node ./scripts/generate-dict.ts
+npx ts-node --compilerOptions '{ "lib": ["es2023"], "module": "node16", "target": "es2022", "strict": true, "esModuleInterop": true, "skipLibCheck": true, "moduleResolution": "node16", "resolveJsonModule": true }' ./index.ts
--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@
 import dictJSON from './output/10000-most-common-words-en-fr-dict.json';
 import type { Dictionary, DictionaryWord } from './types';
 
-export const dictionary: Dictionary = Object.entries(dictJSON).reduce((acc, curr: [string, DictionaryWord[]]) => {
+const entries = Object.entries(dictJSON) as unknown as Array<[string, Array<DictionaryWord>]>;
+export const dictionary: Dictionary = entries.reduce((acc, curr) => {
     acc.set(curr[0], curr[1]);
     return acc;
 }, new Map() as Dictionary);
