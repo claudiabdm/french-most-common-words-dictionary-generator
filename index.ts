6089a0e703f56dc1295a4fac3e5cf86a9aadecb5
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-08-05 21:14:40 +0200

    Fix typescript error

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 
 import dictJSON from './output/10000-most-common-words-en-fr-dict.json';
-import type { Dictionary } from './types';
+import type { Dictionary, DictionaryWord } from './types';
 
-export const dictionary: Dictionary = Object.entries(dictJSON).reduce((acc, curr) => {
+export const dictionary: Dictionary = Object.entries(dictJSON).reduce((acc, curr: [string, DictionaryWord[]]) => {
     acc.set(curr[0], curr[1]);
     return acc;
 }, new Map() as Dictionary);
